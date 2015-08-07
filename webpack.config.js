6a2d6c696131dba1ecc6c10943cd5580d815129a
Benjamin Goh <masotime@gmail.com>
2015-08-06 21:18:23 -0700

    Added CSS and styling

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,24 @@ var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 
 var ROOT_PATH = path.resolve(__dirname);
+function pathTo(rel) {
+	return path.resolve(ROOT_PATH, rel);
+}
 
 module.exports = {
-	entry: path.resolve(ROOT_PATH, 'app/main'),
+	entry: pathTo('app/main'),
 	output: {
-		path: path.resolve(ROOT_PATH, 'build'),
+		path: pathTo('build'),
 		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.css$/, // all files ending with CSS
+				loaders: ['style', 'css'],
+				include: pathTo('app')
+			}
+		]
 	},
 	plugins: [
 		new HtmlwebpackPlugin({
